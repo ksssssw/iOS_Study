dfcb3daba52dfa4f77354ec36edec851d3c8d786
ksssssw <seokwon.kang@onestorecorp.com>
2023-10-06 17:37:48 +0900

    print

---
+++ Swift_Language/MyPlayground.playground/Pages/Consol Log.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let age: Int = 26
+
+print("안녕하세요 저는 \(age)살입니다.")
+print("\n############\n")
+
+class Person{
+    let name = "Sean"
+    let age = 26
+}
+
+let sean = Person()
+print("sean info = \(sean)")
+dump(sean)
