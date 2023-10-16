0f794ea0ad020fd1f22722625cd265eb31740347
ksssssw <seokwon.kang@onestorecorp.com>
2023-10-16 18:24:58 +0900

    10/16 - 랜덤다이스 게임 만들어보기

---
+++ Swift_Language/iOS_Sample_App/DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 1000288 on 2023/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        // 첫번째 이미지뷰의 이미지를 랜덤으로 변경
+        firstImageView.image = diceArray.randomElement()
+        
+        // 두번째 이미지뷰의 이미지를 랜덤으로 변경
+        secondImageView.image = diceArray.randomElement()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
