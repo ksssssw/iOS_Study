a6b4fc479a3dd49ba30e4507eae2e1a2af607f8f
SeokWon <rtd0731@naver.com>
2023-10-24 17:47:28 +0900

    [10/24] MVC 패턴

---
+++ Swift_Language/iOS_Sample_App/BMI/BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by 1000288 on 2023/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
