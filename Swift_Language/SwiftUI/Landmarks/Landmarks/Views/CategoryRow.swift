be3e57b7af928c0ba960ccc6ba99c647f9923b54
SeokWon <rtd0731@naver.com>
2023-10-30 22:48:51 +0900

    [10/30] SwiftUI 듀토리얼 진행중

--- Swift_Language/SwiftUI/Landmarks/Landmarks/Views/CategoryRow.swift
+++ Swift_Language/SwiftUI/Landmarks/Landmarks/Views/CategoryRow.swift
@@ -12,14 +12,32 @@ struct CategoryRow: View {
     var items: [Landmark]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
     }
 }
 
 #Preview {
     let landmarks = ModelData().landmarks
     return CategoryRow(
-        categoryName: landmarks[0].category.rawValue, 
+        categoryName: landmarks[0].category.rawValue,
         items: Array(landmarks.prefix(3))
     )
 }
