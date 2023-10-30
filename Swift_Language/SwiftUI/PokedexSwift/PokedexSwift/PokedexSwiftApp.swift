7f615b9211df9396c932ad19e7060a537d36575a
SeokWon <rtd0731@naver.com>
2023-10-30 22:55:29 +0900

    [10/30] PokdeDex Clean Architecture Start

---
+++ Swift_Language/SwiftUI/PokedexSwift/PokedexSwift/PokedexSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftApp.swift
+//  PokedexSwift
+//
+//  Created by 1000288 on 2023/10/30.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
