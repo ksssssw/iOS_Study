8e5d41000aeaab04d5d5d3c73fa87c9953e26ee8
SeokWon <rtd0731@naver.com>
2023-11-15 17:59:47 +0900

    [11/15] ViewAndModifiersApp

---
+++ Swift_Language/SwiftUI/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by 1000288 on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
