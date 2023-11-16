568cbb7561b611f6da68b0dfc6f3a9beecbe4b03
SeokWon <rtd0731@naver.com>
2023-11-16 16:40:10 +0900

    [11/16] WordScramble

---
+++ Swift_Language/SwiftUI/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by 1000288 on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
