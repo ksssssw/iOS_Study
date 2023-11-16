f3a9842ccc3a74e652231be81a9caa6cd8918834
SeokWon <rtd0731@naver.com>
2023-11-16 10:07:32 +0900

    [11/16] BetterRest

---
+++ Swift_Language/SwiftUI/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by 1000288 on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
