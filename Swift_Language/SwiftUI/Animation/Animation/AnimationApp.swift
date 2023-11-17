a8216a7a11a86a8531bf3b14fa2659f9e5c4ae2b
SeokWon <rtd0731@naver.com>
2023-11-17 10:54:24 +0900

    [11/17] Animation

---
+++ Swift_Language/SwiftUI/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by 1000288 on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift_Language/SwiftUI/Animation/Animation/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by 1000288 on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isShowingRed = false
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+
+// MARK: 뱀처럼 따라오는 애니메이션
+//struct ContentView: View {
+//    let letters = Array("Hello SwiftUI")
+//    @State private var enabled = false
+//    @State private var dragAmount = CGSize.zero
+//
+//    var body: some View {
+//        HStack(spacing: 0) {
+//            ForEach(0..<letters.count, id: \.self) { num in
+//                Text(String(letters[num]))
+//                    .padding(5)
+//                    .font(.title)
+//                    .background(enabled ? .blue : .red)
+//                    .offset(dragAmount)
+//                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+//            }
+//        }
+//        .gesture(
+//            DragGesture()
+//                .onChanged { dragAmount = $0.translation }
+//                .onEnded { _ in
+//                    dragAmount = .zero
+//                    enabled.toggle()
+//                }
+//        )
+//    }
+//}
+
+#Preview {
+    ContentView()
+}
+
+
+// MARK: 빙글빙글 3D돌기!
+//        Button("Tap Me") {
+//            withAnimation(.bouncy(duration: 3)) {
+//                animationAmount += 360
+//            }
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundStyle(.white)
+//        .clipShape(.circle)
+//        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
