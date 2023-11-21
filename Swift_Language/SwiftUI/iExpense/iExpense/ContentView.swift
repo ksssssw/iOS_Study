92180a85420814eda4d1d186e033595fae211dc7
SeokWon <rtd0731@naver.com>
2023-11-21 18:21:02 +0900

    [11/21] 테스트중

--- Swift_Language/SwiftUI/iExpense/iExpense/ContentView.swift
+++ Swift_Language/SwiftUI/iExpense/iExpense/ContentView.swift
@@ -9,31 +9,114 @@ import SwiftUI
 import Observation
 
 struct ContentView: View {
-    @State private var showingSheet = false
-
-    var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "@twostraws")
-        }
-    }
-}
-
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    let name: String
+    @State private var someToggle = true
     
     var body: some View {
-        Text("Hello, \(name)!")
-        Button("Dismiss") {
-            dismiss()
+        ZStack {
+            Color.black
+            
+            Toggle("확인", isOn: $someToggle)
+                .toggleStyle(SwitchLightStyle())
+                .labelsHidden()
         }
     }
 }
 
+struct SwitchLightStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        let isOn = configuration.isOn
+        
+        return ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .frame(width: 37, height: 15)
+                .foregroundColor(isOn ? Color.gray : Color.blue)
+                .overlay(alignment: isOn ? .leading : .trailing) {
+                    Circle()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.white)
+                }
+        }
+        .onTapGesture {
+            withAnimation {
+                configuration.isOn.toggle()
+            }
+        }
+    }
+}
 
-#Preview {
+struct Home: View {
+    @State var index = 0
+    
+    var body: some View {
+        GeometryReader { proxy in
+            ScrollView {
+                VStack {
+                    HomeTabView(index: $index)
+                    
+                    TabView(selection: $index) {
+                        
+                        Color.blue
+                            .tag(0)
+                        
+                        Color.red
+                            .tag(1)
+                    }
+                    .frame(maxWidth: .infinity, minHeight: proxy.size.height)
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                }
+            }
+        }
+        
+    }
+}
+
+struct HomeTabView: View {
+    @Binding var index: Int
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Spacer()
+            
+            Text("Recomm.")
+                .foregroundColor(self.index == 0 ? .white : .black)
+                .fontWeight(.bold)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 20)
+                .background(self.index == 0 ? Color(red: 39.0/255.0, green: 37.0/255.0, blue: 88.0/255.0) : .white)
+                .clipShape(Capsule())
+                .onTapGesture {
+                    withAnimation(.default) {
+                        self.index = 0
+                    }
+                }
+            
+            Text("Top Charts")
+                .foregroundColor(self.index == 1 ? .white : Color.black)
+                .fontWeight(.bold)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 20)
+                .background(self.index == 1 ? Color(red: 39.0/255.0, green: 37.0/255.0, blue: 88.0/255.0) : .white)
+                .clipShape(Capsule())
+                .onTapGesture {
+                    withAnimation(.default) {
+                        self.index = 1
+                    }
+                }
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview("CustomToggle") {
     ContentView()
 }
+
+//#Preview("HomeTabView") {
+//    HomeTabView()
+//}
+
+#Preview("Home") {
+    Home()
+}
+
